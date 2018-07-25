19e7f8c1156046c88be2a65460e9396d7665effb
yinxiupei <xiupeiyin@126.com>
2018-07-25 17:56:29 +0800

    Added: 增加默认页面

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,13 @@ const post = require('./post')
 
 const router = new Router()
 
+router.get('/', async (ctx, next) => {
+  ctx.body = {
+    code: 1,
+    body: 'I am Southern Girl',
+    msg: 'hello world'
+  }
+})
 router.use(post.routes(), post.allowedMethods())
 
 module.exports = router
