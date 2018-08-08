41b87ad83dd8eb3bba5a89a82947649636ccac90
yinxiupei <xiupeiyin@126.com>
2018-08-08 17:41:06 +0800

    Modify:使用cors解决跨域问题

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 const koa = require('koa')
 const bodyparser = require('koa-bodyparser')
 const app = new koa()
-const fs = require('fs')
 const routes = require('./routes')
+const cors = require('koa2-cors')
 // 连接数据库
 const mongoose = require('./config/baseConfig')
 mongoose.connect()
 
-app.use(async (ctx, next) => {
-  ctx.set('Access-Control-Allow-Origin', '*')
-  ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  ctx.set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
-  await next()
-})
+app.use(cors({
+  origin: '*',
+  exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
+  allowMethods: ['POST', 'GET', 'PUT', 'DELETE', 'OPTIONS'],
+  allowHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept']
+}))
+
+// 设置请求头
+// app.use(async (ctx, next) => {
+//   ctx.set('Access-Control-Allow-Origin', '*')
+//   ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+//   ctx.set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
+//   await next()
+// })
 // 错误处理
 const handler = async (ctx, next) => {
   try {
@@ -28,7 +36,7 @@ const handler = async (ctx, next) => {
 
 // 路由
 // routes
-app.use(routes.routes(), routes.allowedMethods());
+app.use(routes.routes(), routes.allowedMethods())
+// 引入koa-bodyparser 它用于解析客户端请求的body中的内容,内部使用JSON编码处理
 app.use(bodyparser)
-// app.use(main)
 app.listen(3000)
